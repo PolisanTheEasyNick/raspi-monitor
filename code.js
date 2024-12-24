8e23167c942bb83bd9f1a49a9e95e10cb7369d3a
Oleh Polisan <polisanoleg@gmail.com>
2024-12-24 17:48:22 +0200

    migrate to rpi_export

--- code.js
+++ code.js
@@ -3,17 +3,17 @@ var Gauge = window.Gauge;
 var cpuUsageGauge = Gauge(document.getElementById("cpuUsageGauge"), {
   max: 100,
   // custom label renderer
-  label: function(value) {
+  label: function (value) {
     return Math.round(value) + "%";
   },
   value: 50,
   // Custom dial colors (Optional)
-  color: function(value) {
-    if(value < 80) {
+  color: function (value) {
+    if (value < 80) {
       return "var(--good)"; // green
-    }else if(value >= 80 && value < 90) {
+    } else if (value >= 80 && value < 90) {
       return "var(--warning)"; // yellow
-    }else if(value >= 90) {
+    } else if (value >= 90) {
       return "var(--bad)"; // red
     }
   },
@@ -24,17 +24,17 @@ var cpuUsageGauge = Gauge(document.getElementById("cpuUsageGauge"), {
 var gpuUsageGauge = Gauge(document.getElementById("gpuUsageGauge"), {
   max: 100,
   // custom label renderer
-  label: function(value) {
+  label: function (value) {
     return Math.round(value) + "%";
   },
   value: 50,
   // Custom dial colors (Optional)
-  color: function(value) {
-    if(value < 80) {
+  color: function (value) {
+    if (value < 80) {
       return "var(--good)"; // green
-    }else if(value >= 80 && value < 90) {
+    } else if (value >= 80 && value < 90) {
       return "var(--warning)"; // yellow
-    }else if(value >= 90) {
+    } else if (value >= 90) {
       return "var(--bad)"; // red
     }
   },
@@ -45,17 +45,17 @@ var gpuUsageGauge = Gauge(document.getElementById("gpuUsageGauge"), {
 var ramUsageGauge = Gauge(document.getElementById("ramUsageGauge"), {
   max: 100,
   // custom label renderer
-  label: function(value) {
+  label: function (value) {
     return Math.round(value) + "%";
   },
   value: 50,
   // Custom dial colors (Optional)
-  color: function(value) {
-    if(value < 80) {
+  color: function (value) {
+    if (value < 80) {
       return "var(--good)"; // green
-    }else if(value >= 80 && value < 90) {
+    } else if (value >= 80 && value < 90) {
       return "var(--warning)"; // yellow
-    }else if(value >= 90) {
+    } else if (value >= 90) {
       return "var(--bad)"; // red
     }
   },
@@ -66,17 +66,17 @@ var ramUsageGauge = Gauge(document.getElementById("ramUsageGauge"), {
 var cpuTempGauge = Gauge(document.getElementById("cpuTempGauge"), {
   max: 100,
   // custom label renderer
-  label: function(value) {
+  label: function (value) {
     return Math.round(value) + "°C";
   },
   value: 50,
   // Custom dial colors (Optional)
-  color: function(value) {
-    if(value < 65) {
+  color: function (value) {
+    if (value < 65) {
       return "var(--good)"; // green
-    }else if(value >= 65 && value < 75) {
+    } else if (value >= 65 && value < 75) {
       return "var(--warning)"; // yellow
-    }else if(value >= 75) {
+    } else if (value >= 75) {
       return "var(--bad)"; // red
     }
   },
@@ -87,17 +87,17 @@ var cpuTempGauge = Gauge(document.getElementById("cpuTempGauge"), {
 var gpuTempGauge = Gauge(document.getElementById("gpuTempGauge"), {
   max: 100,
   // custom label renderer
-  label: function(value) {
+  label: function (value) {
     return Math.round(value) + "°C";
   },
   value: 50,
   // Custom dial colors (Optional)
-  color: function(value) {
-    if(value < 65) {
+  color: function (value) {
+    if (value < 65) {
       return "var(--good)"; // green
-    }else if(value >= 65 && value < 75) {
+    } else if (value >= 65 && value < 75) {
       return "var(--warning)"; // yellow
-    }else if(value >= 75) {
+    } else if (value >= 75) {
       return "var(--bad)"; // red
     }
   },
@@ -111,177 +111,285 @@ const promPC = new Prometheus.PrometheusDriver({
 
 const promRPI = new Prometheus.PrometheusDriver({
   endpoint: prometheusRPIEndpoint,
-})
+});
 
 var MemTotal;
-promPC.instantQuery('node_memory_MemTotal_bytes')
-  .then((res) => {
-	if(res['result'][0]) {
-      MemTotal = res['result'][0]['value']['value']
-	}
-  })
-
+promPC.instantQuery("node_memory_MemTotal_bytes").then((res) => {
+  if (res["result"][0]) {
+    MemTotal = res["result"][0]["value"]["value"];
+  }
+});
 
 function setGaugeValue(gauge, value, elementID, goodValue, warningValue) {
-  gauge.setValueAnimated(value, 0.5)
-  element = document.getElementById(elementID).children["1"].children[1].children[0]
+  gauge.setValueAnimated(value, 0.5);
+  element =
+    document.getElementById(elementID).children["1"].children[1].children[0];
   if (value < goodValue) {
     element.style.fill = "var(--good)";
   } else if (value >= goodValue && value < warningValue) {
     element.style.fill = "var(--warning)";
-  } else if(value >= warningValue) {
+  } else if (value >= warningValue) {
     element.style.fill = "var(--bad)";
   }
-
 }
 
 const updateGaugeValue = () => {
-  promPC.instantQuery('{__name__=~"aida_sys_scpuuti|aida_sys_sgpuuti|nvidia_smi_utilization_gpu_ratio|aida_sys_srtssfps|aida_sys_memuti|node_memory_MemAvailable_bytes|aida_temp|aida_temp_tgpu|nvidia_smi_temperature_gpu|aida_sys_suptime_total|node_boot_time_seconds"}')
+  promPC
+    .instantQuery(
+      '{__name__=~"aida_sys_scpuuti|aida_sys_sgpuuti|nvidia_smi_utilization_gpu_ratio|aida_sys_srtssfps|aida_sys_memuti|node_memory_MemAvailable_bytes|aida_temp|aida_temp_tgpu|nvidia_smi_temperature_gpu|aida_sys_suptime_total|node_boot_time_seconds"}',
+    )
     .then((res) => {
-      for(const metric of res.result) {
-        switch(metric["metric"]["name"]) {
-          case 'aida_sys_memuti':
-            setGaugeValue(ramUsageGauge, metric["value"]["value"], "ramUsageGauge", 80, 90)
-            break
-          case 'aida_sys_scpuuti':
-            setGaugeValue(cpuUsageGauge, metric["value"]["value"], "cpuUsageGauge", 80, 90)
-            break
-          case 'nvidia_smi_utilization_gpu_ratio':
+      console.log("Then in prom PC instant query, result is: ", res);
+      for (const metric of res.result) {
+        switch (metric["metric"]["name"]) {
+          case "aida_sys_memuti":
+            setGaugeValue(
+              ramUsageGauge,
+              metric["value"]["value"],
+              "ramUsageGauge",
+              80,
+              90,
+            );
+            break;
+          case "aida_sys_scpuuti":
+            setGaugeValue(
+              cpuUsageGauge,
+              metric["value"]["value"],
+              "cpuUsageGauge",
+              80,
+              90,
+            );
+            break;
+          case "nvidia_smi_utilization_gpu_ratio":
             metric["value"]["value"] *= 100;
-          case 'aida_sys_sgpuuti':
-            setGaugeValue(gpuUsageGauge, metric["value"]["value"], "gpuUsageGauge", 80, 90)
-            break
-          case 'aida_sys_srtssfps':
-            document.getElementById("fps").textContent = metric["value"]["value"];
-            if(metric["value"]["value"] == 0) {
-              document.getElementById("fps").style.color = 'white'
-            } else if(metric["value"]["value"] > 0 && metric["value"]["value"] < 30) {
-              document.getElementById("fps").style.color = 'var(--bad)'
-            } else if(metric["value"]["value"] >= 30 && metric["value"]["value"] < 50) {
-              document.getElementById("fps").style.color = 'var(--warning)'
+          case "aida_sys_sgpuuti":
+            setGaugeValue(
+              gpuUsageGauge,
+              metric["value"]["value"],
+              "gpuUsageGauge",
+              80,
+              90,
+            );
+            break;
+          case "aida_sys_srtssfps":
+            document.getElementById("fps").textContent =
+              metric["value"]["value"];
+            if (metric["value"]["value"] == 0) {
+              document.getElementById("fps").style.color = "white";
+            } else if (
+              metric["value"]["value"] > 0 &&
+              metric["value"]["value"] < 30
+            ) {
+              document.getElementById("fps").style.color = "var(--bad)";
+            } else if (
+              metric["value"]["value"] >= 30 &&
+              metric["value"]["value"] < 50
+            ) {
+              document.getElementById("fps").style.color = "var(--warning)";
             } else {
-              document.getElementById("fps").style.color = 'var(--good)'
+              document.getElementById("fps").style.color = "var(--good)";
             }
-            break
-          case 'aida_sys_suptime_total':
+            break;
+          case "aida_sys_suptime_total":
             const date = new Date(null);
             date.setSeconds(metric["value"]["value"]);
             const result = date.toISOString().slice(11, 19);
-            document.getElementById("uptime").textContent = result
-            break
-          case 'aida_temp':
-            setGaugeValue(cpuTempGauge, metric["value"]["value"], "cpuTempGauge", 65, 75)
-            break
-          case 'aida_temp_tgpu':
-          case 'nvidia_smi_temperature_gpu':
-            setGaugeValue(gpuTempGauge, metric["value"]["value"], "gpuTempGauge", 65, 75)
-            break
-          case 'node_boot_time_seconds':
+            document.getElementById("uptime").textContent = result;
+            break;
+          case "aida_temp":
+            setGaugeValue(
+              cpuTempGauge,
+              metric["value"]["value"],
+              "cpuTempGauge",
+              65,
+              75,
+            );
+            break;
+          case "aida_temp_tgpu":
+          case "nvidia_smi_temperature_gpu":
+            setGaugeValue(
+              gpuTempGauge,
+              metric["value"]["value"],
+              "gpuTempGauge",
+              65,
+              75,
+            );
+            break;
+          case "node_boot_time_seconds":
             const date2 = new Date();
-            date2.setTime(new Date().getTime() - metric["value"]["value"] * 1000);
+            date2.setTime(
+              new Date().getTime() - metric["value"]["value"] * 1000,
+            );
             const result2 = date2.toISOString().slice(11, 19);
-            document.getElementById("uptime").textContent = result2
-            break
-          case 'node_memory_MemAvailable_bytes':
-            const memUsage = (1 - (metric["value"]["value"] / (MemTotal)))* 100
-            ramUsageGauge.setValueAnimated(memUsage, 0.5)
-            element = document.getElementById("ramUsageGauge").children["1"].children[1].children[0]
+            document.getElementById("uptime").textContent = result2;
+            break;
+          case "node_memory_MemAvailable_bytes":
+            const memUsage = (1 - metric["value"]["value"] / MemTotal) * 100;
+            ramUsageGauge.setValueAnimated(memUsage, 0.5);
+            element =
+              document.getElementById("ramUsageGauge").children["1"].children[1]
+                .children[0];
             if (memUsage < 80) {
               element.style.fill = "var(--good)";
             } else if (memUsage >= 80 && memUsage < 90) {
               element.style.fill = "var(--warning)";
-            } else if(memUsage >= 90) {
+            } else if (memUsage >= 90) {
               element.style.fill = "var(--bad)";
             }
-            break
+            break;
 
           default:
-            console.log(metric["metric"]["name"]) 
+            console.log(metric["metric"]["name"]);
         }
       }
     })
     .catch((err) => {
-      console.error('Error fetching Prometheus data: ' + err);
+      console.error("Error fetching Prometheus data: " + err);
     });
 
-    //cpu utilization
-    promPC.instantQuery("100 - (avg(irate(node_cpu_seconds_total{mode='idle'}[1m])) * 100)")
+  //cpu utilization
+  promPC
+    .instantQuery(
+      "100 - (avg(irate(node_cpu_seconds_total{mode='idle'}[1m])) * 100)",
+    )
     .then((res) => {
-	  if(res['result'][0]) {
-         setGaugeValue(cpuUsageGauge, res['result'][0]['value']['value'], "cpuUsageGauge", 80, 90)
-	  }
-    })
-    promPC.instantQuery(`node_hwmon_temp_celsius{sensor='${tempSensor}'}`)
+      if (res["result"][0]) {
+        setGaugeValue(
+          cpuUsageGauge,
+          res["result"][0]["value"]["value"],
+          "cpuUsageGauge",
+          80,
+          90,
+        );
+      }
+    });
+  promPC
+    .instantQuery(`node_hwmon_temp_celsius{sensor='${tempSensor}'}`)
     .then((res) => {
-	  if(res['result'][0]) {
-		if(res['result'][0]) {
-          setGaugeValue(cpuTempGauge, res['result'][0]['value']['value'], "cpuTempGauge", 65, 75)
-		}
-	  }
-    })
+      if (res["result"][0]) {
+        if (res["result"][0]) {
+          setGaugeValue(
+            cpuTempGauge,
+            res["result"][0]["value"]["value"],
+            "cpuTempGauge",
+            65,
+            75,
+          );
+        }
+      }
+    });
 };
 
 const updateRpiValue = () => {
-  promRPI.instantQuery("rpi_cpu_temperature_celsius")
+  promRPI
+    .instantQuery("rpi_temperature_c")
     .then((res) => {
-      document.getElementById("rpi-temp").textContent = Math.round(res.result[0]["value"]["value"] * 10) / 10
-      if(res.result[0]["value"]["value"] > 0 && res.result[0]["value"]["value"] < 65) {
-        document.getElementById("rpi-temp").style.color = 'var(--good)'
-      } else if(res.result[0]["value"]["value"] >= 65 && res.result[0]["value"]["value"] < 75) {
-        document.getElementById("rpi-temp").style.color = 'var(--warning)'
+      document.getElementById("rpi-temp").textContent =
+        Math.round(res.result[0]["value"]["value"] * 10) / 10;
+      if (
+        res.result[0]["value"]["value"] > 0 &&
+        res.result[0]["value"]["value"] < 65
+      ) {
+        document.getElementById("rpi-temp").style.color = "var(--good)";
+      } else if (
+        res.result[0]["value"]["value"] >= 65 &&
+        res.result[0]["value"]["value"] < 75
+      ) {
+        document.getElementById("rpi-temp").style.color = "var(--warning)";
       } else {
-        document.getElementById("rpi-temp").style.color = 'var(--bad)'
+        document.getElementById("rpi-temp").style.color = "var(--bad)";
       }
     })
     .catch((err) => {
-      console.error('Error fetching Prometheus data:' + err);
+      console.error("Error fetching Prometheus data:" + err);
     });
-}
+};
 // Set up an interval to call the update function every second
 const queryInterval1 = setInterval(updateGaugeValue, 1000);
 const queryInterval2 = setInterval(updateRpiValue, 1000);
 
 //WEATHER
 async function getWeatherWS() {
-    let socket;
+  let socket;
 
-    async function connectWebSocket() {
-        socket = new WebSocket(websocketWeatherServer);
+  async function connectWebSocket() {
+    socket = new WebSocket(websocketWeatherServer);
 
-        socket.addEventListener('open', () => {
-            console.log('WebSocket connected');
-        });
+    socket.addEventListener("open", () => {
+      console.log("WebSocket connected");
+    });
 
-        socket.addEventListener('message', async function (event) {
-            try {
-                const weatherData = JSON.parse(event.data);
+    socket.addEventListener("message", async function (event) {
+      try {
+        const weatherData = JSON.parse(event.data);
 
-                // Update the UI with new weather data
-                const temperature = Math.round(weatherData.temperature_celsius);
-                const description = weatherData.weather_text;
-                const weatherIcon = weatherData.weather_icon_id;
+        // Update the UI with new weather data
+        const temperature = Math.round(weatherData.temperature_celsius);
+        const description = weatherData.weather_text;
+        const weatherIcon = weatherData.weather_icon_id;
 
-                document.querySelector('.temp-number').textContent = temperature;
-                document.querySelector('.description').textContent = description;
-                document.getElementById('uvi').textContent = weatherData.uv_index;
+        document.querySelector(".temp-number").textContent = temperature;
+        document.querySelector(".description").textContent = description;
+        document.getElementById("uvi").textContent = weatherData.uv_index;
 
-                const svgResponse = await fetch(weatherIconFolderLink + weatherIcon + '.svg');
-                const svgText = await svgResponse.text();
+        const svgResponse = await fetch(
+          weatherIconFolderLink + weatherIcon + ".svg",
+        );
+        const svgText = await svgResponse.text();
 
-                const svgContainer = document.querySelector('.weather-icon');
-                svgContainer.innerHTML = svgText;
-            } catch (error) {
-                console.error('Error parsing WebSocket data', error);
-            }
-        });
+        const svgContainer = document.querySelector(".weather-icon");
+        svgContainer.innerHTML = svgText;
+      } catch (error) {
+        console.error("Error parsing WebSocket data", error);
+      }
+    });
 
-        socket.addEventListener('close', (event) => {
-            console.error('WebSocket closed. Reconnecting in 10 seconds...');
-            setTimeout(connectWebSocket, 10000); // Reconnect after 10 seconds
-        });
-    }
+    socket.addEventListener("close", (event) => {
+      console.error("WebSocket closed. Reconnecting in 10 seconds...");
+      setTimeout(connectWebSocket, 10000); // Reconnect after 10 seconds
+    });
+  }
 
-    // Initial connection attempt
-    await connectWebSocket();
+  // Initial connection attempt
+  await connectWebSocket();
 }
-getWeatherWS()
+getWeatherWS();
+
+//FPS From linux desktop
+async function getFPSWS() {
+  let socket;
+
+  async function connectWebSocket() {
+    socket = new WebSocket(websocketFPSServer);
+
+    socket.addEventListener("open", () => {
+      console.log("WebSocket FPS connected");
+    });
+
+    socket.addEventListener("message", async function (event) {
+      try {
+        const FPS = Math.round(event.data);
+        if (FPS >= 50 || FPS == 0) {
+          document.getElementById("fps").style.color = "var(--good)";
+        } else if (FPS >= 30 && FPS < 50) {
+          document.getElementById("fps").style.color = "var(--warning)";
+        } else {
+          document.getElementById("fps").style.color = "var(--bad)";
+        }
+
+        document.getElementById("fps").textContent = FPS;
+      } catch (error) {
+        console.error("Error parsing WebSocket data", error);
+      }
+    });
+
+    socket.addEventListener("close", (event) => {
+      console.error("WebSocket closed. Reconnecting in 10 seconds...");
+      setTimeout(connectWebSocket, 10000); // Reconnect after 10 seconds
+    });
+  }
+
+  // Initial connection attempt
+  await connectWebSocket();
+}
+getFPSWS();
--- pcoff.js
+++ pcoff.js
@@ -1,119 +1,135 @@
 function formatTime(seconds) {
-    const days = Math.floor(seconds / (24 * 3600));
-    const hours = Math.floor((seconds % (24 * 3600)) / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
-    const remainingSeconds = seconds % 60;
-  
-    const formattedTime =
-      (days > 0 ? days + "d " : "") +
-      (hours < 10 ? "0" : "") +
-      hours +
-      ":" +
-      (minutes < 10 ? "0" : "") +
-      minutes +
-      ":" +
-      (remainingSeconds < 10 ? "0" : "") +
-      remainingSeconds;
-  
-    return formattedTime;
-  }
+  const days = Math.floor(seconds / (24 * 3600));
+  const hours = Math.floor((seconds % (24 * 3600)) / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = seconds % 60;
+
+  const formattedTime =
+    (days > 0 ? days + "d " : "") +
+    (hours < 10 ? "0" : "") +
+    hours +
+    ":" +
+    (minutes < 10 ? "0" : "") +
+    minutes +
+    ":" +
+    (remainingSeconds < 10 ? "0" : "") +
+    remainingSeconds;
+
+  return formattedTime;
+}
 
 async function getWeatherWS() {
-    let socket;
+  let socket;
 
-    async function connectWebSocket() {
-        socket = new WebSocket(websocketWeatherServer);
+  async function connectWebSocket() {
+    socket = new WebSocket(websocketWeatherServer);
 
-        socket.addEventListener('open', () => {
-            console.log('WebSocket connected');
-        });
+    socket.addEventListener("open", () => {
+      console.log("WebSocket connected");
+    });
 
-        socket.addEventListener('message', async function (event) {
-            try {
-                console.log('Got event: ', event);
-                console.log('event.data: ', event.data);
+    socket.addEventListener("message", async function (event) {
+      try {
+        console.log("Got event: ", event);
+        console.log("event.data: ", event.data);
 
-                const weatherData = JSON.parse(event.data);
+        const weatherData = JSON.parse(event.data);
 
-                const temperature = Math.round(weatherData.temperature_celsius);
-                const weatherIcon = weatherData.weather_icon_id;
-                const uv_index = weatherData.uv_index
-                const weatherDescription = weatherData.weather_text;
-                document.querySelector('.temp-number').textContent = temperature;
-                document.querySelector('.weather-description').textContent = weatherDescription;
-                document.querySelector('.temp-number').textContent = temperature;
-                document.querySelector('.uv-number').textContent = uv_index;
+        const temperature = Math.round(weatherData.temperature_celsius);
+        const weatherIcon = weatherData.weather_icon_id;
+        const uv_index = weatherData.uv_index;
+        const weatherDescription = weatherData.weather_text;
+        document.querySelector(".temp-number").textContent = temperature;
+        document.querySelector(".weather-description").textContent =
+          weatherDescription;
+        document.querySelector(".temp-number").textContent = temperature;
+        document.querySelector(".uv-number").textContent = uv_index;
 
-                const svgResponse = await fetch(weatherIconFolderLink + weatherIcon + '.svg');
-                const svgText = await svgResponse.text();
+        const svgResponse = await fetch(
+          weatherIconFolderLink + weatherIcon + ".svg",
+        );
+        const svgText = await svgResponse.text();
 
-                const svgContainer = document.querySelector('.weather-icon');
-                svgContainer.innerHTML = svgText;
-            } catch (error) {
-                console.error('Error parsing WebSocket data', error);
-            }
-        });
+        const svgContainer = document.querySelector(".weather-icon");
+        svgContainer.innerHTML = svgText;
+      } catch (error) {
+        console.error("Error parsing WebSocket data", error);
+      }
+    });
 
-        socket.addEventListener('close', (event) => {
-            console.error('WebSocket closed. Reconnecting in 10 seconds...');
-            setTimeout(connectWebSocket, 10000); // Reconnect after 10 seconds
-        });
-    }
-    await connectWebSocket();
+    socket.addEventListener("close", (event) => {
+      console.error("WebSocket closed. Reconnecting in 10 seconds...");
+      setTimeout(connectWebSocket, 10000); // Reconnect after 10 seconds
+    });
+  }
+  await connectWebSocket();
 }
-getWeatherWS()
+getWeatherWS();
 
 function updateDateTime() {
-    const currentDate = new Date();
+  const currentDate = new Date();
 
-    // Format date as "DD Month YYYY"
-    const optionsDate = { day: '2-digit', month: 'long', year: 'numeric' };
-    const formattedDate = currentDate.toLocaleDateString('en-US', optionsDate);
+  // Format date as "DD Month YYYY"
+  const optionsDate = { day: "2-digit", month: "long", year: "numeric" };
+  const formattedDate = currentDate.toLocaleDateString("en-US", optionsDate);
 
-    // Format time as "HH:mm:ss"
-    const optionsTime = { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
-    const formattedTime = currentDate.toLocaleTimeString('en-US', optionsTime);
+  // Format time as "HH:mm:ss"
+  const optionsTime = {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  };
+  const formattedTime = currentDate.toLocaleTimeString("en-US", optionsTime);
 
-    // Update elements with the formatted date and time
-    document.getElementById('date').textContent = formattedDate;
-    document.getElementById('time').textContent = formattedTime;
-  }
+  // Update elements with the formatted date and time
+  document.getElementById("date").textContent = formattedDate;
+  document.getElementById("time").textContent = formattedTime;
+}
 
-  // Update date and time every second
-  setInterval(updateDateTime, 1000);
+// Update date and time every second
+setInterval(updateDateTime, 1000);
 
-  // Initial update when the page loads
-  updateDateTime();
+// Initial update when the page loads
+updateDateTime();
 
 const promRPI = new Prometheus.PrometheusDriver({
-    endpoint: prometheusRPIEndpoint,
-  })
+  endpoint: prometheusRPIEndpoint,
+});
 
-  const updateRpiValue = () => {
-    promRPI.instantQuery('{__name__=~"rpi_cpu_temperature_celsius|node_boot_time_seconds"}')
-      .then((res) => {
-        for(const metric of res.result) {
-            switch(metric["metric"]["name"]) {
-                case 'rpi_cpu_temperature_celsius': 
-                    document.getElementById("rpi-temp").textContent = Math.round(metric["value"]["value"] * 10) / 10 + "°"
-                    if(metric["value"]["value"] > 0 && metric["value"]["value"] < 65) {
-                      document.getElementById("rpi-temp").style.color = 'var(--good)'
-                    } else if(metric["value"]["value"] >= 65 && metric["value"]["value"] < 75) {
-                      document.getElementById("rpi-temp").style.color = 'var(--warning)'
-                    } else {
-                      document.getElementById("rpi-temp").style.color = 'var(--bad)'
-                    }
-                    break
-                case 'node_boot_time_seconds':
-                    document.getElementById("uptime").textContent = formatTime(Math.floor(new Date().getTime() / 1000) - metric["value"]["value"])
-                    break
-                
+const updateRpiValue = () => {
+  promRPI
+    .instantQuery('{__name__=~"rpi_temperature_c|node_boot_time_seconds"}')
+    .then((res) => {
+      for (const metric of res.result) {
+        switch (metric["metric"]["name"]) {
+          case "rpi_temperature_c":
+            document.getElementById("rpi-temp").textContent =
+              Math.round(metric["value"]["value"] * 10) / 10 + "°";
+            if (metric["value"]["value"] > 0 && metric["value"]["value"] < 65) {
+              document.getElementById("rpi-temp").style.color = "var(--good)";
+            } else if (
+              metric["value"]["value"] >= 65 &&
+              metric["value"]["value"] < 75
+            ) {
+              document.getElementById("rpi-temp").style.color =
+                "var(--warning)";
+            } else {
+              document.getElementById("rpi-temp").style.color = "var(--bad)";
             }
+            break;
+          case "node_boot_time_seconds":
+            document.getElementById("uptime").textContent = formatTime(
+              Math.floor(new Date().getTime() / 1000) -
+                metric["value"]["value"],
+            );
+            break;
         }
-      })
-      .catch((err) => {
-        console.error('Error fetching Prometheus data:' + err);
-      });
-  }
+      }
+    })
+    .catch((err) => {
+      console.error("Error fetching Prometheus data:" + err);
+    });
+};
 
-  const queryInterval = setInterval(updateRpiValue, 1000);
+const queryInterval = setInterval(updateRpiValue, 1000);
